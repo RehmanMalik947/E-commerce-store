3e17021f8030d815e9b0dde151236876b2ba4752
RehmanMalik947 <its.ab47@gmail.com>
2025-02-09 17:26:38 +0500

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Category from './category';
+import axios from 'axios';
+
 
 function App() {
   const [count, setCount] = useState(0);
+  // let [finalCtegory,setFinalCategory]=useState([]);
+  // const getCategory=()=>
+  // {
+  //   axios.get('https://dummyjson.com/products')
+  //   .then((res)=>res.data)
+  //   .then((finalres)=>{setFinalCategory(finalres)})
+  // }
+  // useEffect(() => {
+  //   getCategory();
+  // }, [])
+
+  
+
+   
 
   return (
     <>
     <div className='py-[50px] bg-gray-100'>
-      <div className='max-w-[1320px] mx-auto ml-20 bg-red-200'>
+      <div className='max-w-[1320px] mx-auto ml-20 '>
         <h1 className='text-center text-[40px] font-bold mb-[30px]'>Our Products</h1>
         <div className='grid grid-cols-[30%_auto] gap-[20px]'>
           <div className='bg-blue-200'>
-              <Category/>
+              {/* <Category finalCtegory={finalCtegory}/> */}
           </div>
           
           <div>
             <div className='grid grid-cols-4 gap-2 ' >
-              <div className='shadow-lg text-center h-auto w-48 object-contain pb-3'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div>
-
-              <div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div>
-
-              <div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div>
-              <div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div><div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div><div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div><div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div>
-
-              <div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div>
-              <div className='shadow-lg text-center h-auto w-48 object-cover pb-4'>
-                <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
-                <h3>Butterfly Necklace</h3>
-                <h3>Rs.1250</h3>
-              </div>
-
+                <ProductItems/>
+                <ProductItems/>
+                <ProductItems/>
+                <ProductItems/>
+                <ProductItems/>
             </div>
           </div>
         </div>
@@ -78,3 +53,14 @@ function App() {
 }
 
 export default App;
+
+function ProductItems()
+{
+  return(
+    <div className='shadow-lg text-center h-auto w-48 object-contain pb-3'>
+    <img src='https://wearjewelery.com/cdn/shop/files/IMG_2013.heic?v=1734114252&width=1445' ></img>
+    <h3>Butterfly Necklace</h3>
+    <h3>Rs.1250</h3>
+  </div>
+  )
+}
--- src/category.jsx
+++ src/category.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-export default function Category() {
+export default function Category({finalCtegory}) {
+    // let cat=finalCtegory.map((v,i)=>{
+    // return(
+    //     <li key={i} className='bg-gray-400 p-[9px] cursor-pointer font-serif font-[200] mb-2'>
+    //         {v}
+    //     </li>
+    // )
+    // })
+
   return (
     <div >
       <h3 className='p-[10px] text-[23px] font-[500]'>
       Product Category</h3>
       <ul>
-        <li className='bg-gray-400 p-[9px] cursor-pointer font-serif font-[200] mb-2'>Bracelet</li>
-        <li className='bg-gray-400 p-[9px] cursor-pointer font-serif font-[200] mb-2'>Bracelet</li>
-        <li className='bg-gray-400 p-[9px] cursor-pointer font-serif font-[200] mb-2'>Bracelet</li>
-        <li className='bg-gray-400 p-[9px] cursor-pointer font-serif font-[200] mb-2'>Bracelet</li>
-        <li className='bg-gray-400 p-[9px] cursor-pointer font-serif font-[200] mb-2'>Bracelet</li>
+            {/* {cat} */}
       </ul>
     </div>
   );
